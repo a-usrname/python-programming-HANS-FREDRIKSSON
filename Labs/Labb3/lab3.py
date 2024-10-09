8a433435128ca0b81e1424303925838e4e5ec340
Hans Fredriksson <hans.fredriksson@iths.se>
2024-10-09 14:19:43 +0200

    initial commit lab3

---
+++ Labs/Labb3/lab3.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Laboration3 - linjär klassificering
+
+# df data frame for the unlabelled data
+df = pd.read_csv('unlabelled_data.csv')
+df.columns = ['X', 'Y']
+
+
+
